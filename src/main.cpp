4e8100cdd834f189c4a76603f485ee5e1833ffda
lvm030876 <lvm030876@gmail.com>
2022-06-01 12:56:08 +0300

    delete wifimanadger

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266httpUpdate.h>
-#include "WiFiManager.h"
+// #include "WiFiManager.h"
 #include <WiFiUdp.h>
 #include "eepromapi.h"
 #include "relay.h"
